965c1d9a3e5ae44ac80167ebd5a8f9ec82f71f5b
ShubhamFunctionUp <shubham9638464483@gmail.com>
2022-07-28 19:12:44 +0530

    auth

--- src/route/route.js
+++ src/route/route.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express();
 const UserController = require('../controller/user')
 const EventController = require('../controller/event')
+const Middleware = require('../middleware/auth')
 router.get('/',async ()=>(console.log("router")))
 
 //   ------------------------------------- User-------------------------------------------------------
 router.post('/registration',UserController.createUser)
 router.post('/login',UserController.login)
 router.post('/logout',UserController.logout)
-router.put('/changePassword',UserController.changePassword)
+router.put('/changePassword',Middleware.authorization,UserController.changePassword)
 router.put('/updatePassword',UserController.updatePassword)
 
 // -------------------------Event------------------------------------------------------------------------
